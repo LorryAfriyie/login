ff7dd5a05ca499eff1a1c850599b40c4cb643217
Lawrence Afriyie <lawrenceafriyie35@gmail.com>
2024-02-08 04:30:15 +0200

    Registration form
    Creating registration form and encapsulating the elements within its dedicated component.

--- src/views/ForgotPassword.jsx
+++ src/views/ForgotPassword.jsx
@@ -1,9 +1,19 @@
 import { Link } from "react-router-dom";
+import { Input } from "../components/input";
 
 export const ForgotPassword = () => {
   return (
     <div>
       <h2>Forgot Password</h2>
+      <form action="">
+        <Input
+          type={"email"}
+          name={"email"}
+          id={"email"}
+          className={""}
+          placeholder={"Enter your email"}
+        />
+      </form>
       <Link to={"/"}>Return to sign-in</Link>
     </div>
   );
--- src/views/RegistrationForm.jsx
+++ src/views/RegistrationForm.jsx
@@ -1,10 +1,74 @@
 import { Link } from "react-router-dom";
+import { Input } from "../components/input";
 
 export const RegistrationForm = () => {
+  const register = (e) => {
+    e.preventDefault();
+
+    alert("Welcome");
+  };
   return (
-    <div className="registration_form">
-      <h2>Registration Form</h2>
-      <Link to={"/"}>Return to sign-in</Link>
+    <div className="register_section">
+      <h2>Registration</h2>
+      <form className="registration-form" onSubmit={register}>
+        <div className="firstname">
+          <Input
+            type={"text"}
+            name={"firstname"}
+            id={"firstname"}
+            className={""}
+            placeholder={"Enter your first name"}
+          />
+        </div>
+
+        <div className="surname">
+          <Input
+            type={"text"}
+            name={"surname"}
+            id={"surname"}
+            className={""}
+            placeholder={"Enter your surname"}
+          />
+        </div>
+
+        <div className="user_email">
+          <Input
+            type={"email"}
+            name={"email"}
+            id={"email"}
+            className={""}
+            placeholder={"Enter your email address"}
+          />
+        </div>
+
+        <div className="user_password">
+          <Input
+            type={"password"}
+            name={"password"}
+            id={"password"}
+            className={""}
+            placeholder={"Enter your password"}
+          />
+        </div>
+
+        <div className="user_confirm_password">
+          <Input
+            type={"password"}
+            name={"confirm_password"}
+            id={"confirm_password"}
+            className={""}
+            placeholder={"Confirm your password"}
+          />
+        </div>
+
+        <div className="submit_btn">
+          <button type="submit">Register</button>
+        </div>
+      </form>
+
+      <div className="link">
+        <Link to={"/"}>Return to sign-in</Link>
+      </div>
     </div>
   );
 };
