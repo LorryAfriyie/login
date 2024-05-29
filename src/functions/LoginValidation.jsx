c11ed083ffa89c00f2dd6a0a888a6b4b57224f93
Lawrence Afriyie <lawrenceafriyie35@gmail.com>
2024-05-29 04:08:53 +0200

    Removing unused code

--- src/functions/LoginValidation.jsx
+++ src/functions/LoginValidation.jsx
@@ -10,21 +10,6 @@ export let CheckLoginInputs = (message, email, password) => {
 
   if (!validateEmail(email.current.value.trim()))
     setErrorBorder(email.current, "Email is not valid.");
-
-  /* // Check if the login and pasword state variables are empty
-  if ((data.email && data.password) === "") {
-    email.current.classList.add("error");
-    password.current.classList.add("error");
-    showModal("Login Error", "Textboxes are empty");
-  } else {
-    message.current.innerHTML = " ";
-    email.current.classList.remove("error");
-    password.current.classList.remove("error");
-  }
-
-  // Check if the state variable values match the record of stored email and password
-  if (data.email === people[0].email && data.password === people[0].password)
-    message.current.innerHTML = `Hello ${people[0].name}`; */
 };
 
 function validateEmail(email) {
