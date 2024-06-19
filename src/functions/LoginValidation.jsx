02867fd4ef47acf842175b3157be847e23abdf18
Lawrence Afriyie <lawrenceafriyie35@gmail.com>
2024-06-19 05:38:00 +0200

    Code refactor
    Creating file dedicated to email verification function and adjusting email error correction logic.

--- src/functions/LoginValidation.jsx
+++ src/functions/LoginValidation.jsx
@@ -1,4 +1,5 @@
 import { setErrorBorder } from "./SetBorder";
+import { validateEmail } from "./ValidateEmail";
 
 export let CheckLoginInputs = (message, email, password) => {
   if (email.current.value.trim() === "") {
@@ -11,11 +12,3 @@ export let CheckLoginInputs = (message, email, password) => {
   if (!validateEmail(email.current.value.trim()))
     setErrorBorder(email.current, "Email is not valid.");
 };
-
-function validateEmail(email) {
-  return String(email)
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    );
-}
--- src/functions/SetBorder.jsx
+++ src/functions/SetBorder.jsx
@@ -1,15 +1,31 @@
+import { validateEmail } from "./ValidateEmail";
+
 export const setErrorBorder = (input, errorMessage) => {
   input.parentElement.className = "form-control error";
   input.placeholder = errorMessage;
 };
 
 export const removeErrorBorder = (input, placeholder) => {
-  if (input.current.parentElement.className === "form-control error") {
+  if (
+    input.current.parentElement.className === "form-control error" &&
+    input.current.name != "email"
+  ) {
     input.current.addEventListener("input", () => {
       input.current.parentElement.className = "form-control";
       input.current.placeholder = placeholder;
     });
   }
+
+  if (
+    input.current.name === "email" &&
+    input.current.parentElement.className === "form-control error"
+  )
+    input.current.addEventListener("input", () => {
+      if (validateEmail(input.current.value)) {
+        input.current.parentElement.className = "form-control";
+        input.current.placeholder = placeholder;
+      }
+    });
 };
 
 export const setSuccessBorder = () => {};
